506b75a0657d139a89f2624b728b18a98351dac2
Udaykiran <108007033+udaykiran927@users.noreply.github.com>
2022-09-26 02:19:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ def predict():
     int_features=[int(i) for i in request.form.values()]
     print(int_features)
     features=[np.array(int_features)]
-    prediction=model.predict(features)
-    return render_template("index.html",house_price="The House Price will be around: {}".format(prediction))
+    prediction=model.predict(features)[0]
+    return render_template("index.html",house_price="The House would cost around: ₹{}".format(prediction))
         
 if __name__=='__main__':
     app.run(debug=True)
