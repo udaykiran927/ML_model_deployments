eec4019534abb492addf8244dcf0b31012a11943
Udaykiran <108007033+udaykiran927@users.noreply.github.com>
2022-09-25 23:25:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ model=pickle.load(open("model.pkl","rb"))
 def home():
     return render_template("index.html")
 
-@app.route("/predict",methods=["GET","POST"])
+@app.route("/predict",methods=["POST"])
 
 def predict():
     int_features=[int(i) for i in request.form.values()]
